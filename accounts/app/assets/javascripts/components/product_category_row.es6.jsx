1bcb38a05a7852cc82b60862a804ff76063a831f
Anh Nguyen <adnguyen1989@gmai.com>
2017-01-03 21:12:40 +0700

    filterable product table demo

---
+++ accounts/app/assets/javascripts/components/product_category_row.es6.jsx
@@ -0,0 +1,7 @@
+class ProductCategoryRow extends React.Component {
+  render() {
+    return (
+      <tr><th colSpan="2">{this.props.category}</th></tr>
+    )
+  }
+}
---
+++ accounts/app/assets/javascripts/components/product_table.es6.jsx
@@ -0,0 +1,30 @@
+class ProductTable extends React.Component {
+  render () {
+    var rows = [];
+    var last_category = null;
+    this.props.products.forEach((product) => {
+      if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)){
+        return;
+      }
+
+      if (product.category != last_category) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      last_category = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
+
